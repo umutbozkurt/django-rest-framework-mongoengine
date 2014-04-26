963e1c537f35e03d9d841724a76bd3f9c2a6ef19
Umut Bozkurt <umut.bozkurt@n11.com>
2014-04-26 11:24:06 +0300

    setup and manifest added for release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-rest-framework-mongoengine',
+    version='1.0',
+    packages=['MongoEngineSerializer',],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+    url='https://github.com/umutbozkurt/django-rest-framework-mongoengine',
+    author='Umut Bozkurt',
+    author_email='umutbozkurt92@gmail.com'
+)
