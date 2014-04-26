414ee6db8b04da967c57278504bff3123226f7fb
Umut Bozkurt <umut.bozkurt@n11.com>
2014-04-26 12:18:23 +0300

    alterations for pypi upload

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='django-rest-framework-mongoengine',
     version='1.0',
     packages=['MongoEngineSerializer',],
-    license=open('LICENSE').read(),
-    long_description=open('README.md').read(),
+    license='see https://github.com/umutbozkurt/django-rest-framework-mongoengine/blob/master/LICENSE',
+    long_description='see https://github.com/umutbozkurt/django-rest-framework-mongoengine/blob/master/README.md',
     url='https://github.com/umutbozkurt/django-rest-framework-mongoengine',
     author='Umut Bozkurt',
     author_email='umutbozkurt92@gmail.com'
