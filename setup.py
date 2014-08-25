a978aa9f9bd3c61450bfc0b26d0d8cb22d5d637f
Umut Bozkurt <umut.bozkurt@n11.com>
2014-08-25 09:02:56 +0300

    setup.py fixed for pypi

--- setup.py
+++ setup.py
@@ -3,11 +3,25 @@ from distutils.core import setup
 setup(
     name='django-rest-framework-mongoengine',
     version='1.3.3',
+    description='Model Serializer that supports MongoEngine, for Django Rest Framework.',
     packages=['rest_framework_mongoengine',],
     license='see https://github.com/umutbozkurt/django-rest-framework-mongoengine/blob/master/LICENSE',
     long_description='see https://github.com/umutbozkurt/django-rest-framework-mongoengine/blob/master/README.md',
     url='https://github.com/umutbozkurt/django-rest-framework-mongoengine',
+    download_url='https://github.com/umutbozkurt/django-rest-framework-mongoengine/releases/tag/v1.3.3',
+    keywords=['mongoengine', 'serializer', 'django rest framework'],
     author='Umut Bozkurt',
     author_email='umutbozkurt92@gmail.com',
-    requires=['mongoengine', 'djangorestframework']
+    requires=['mongoengine', 'djangorestframework'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 'Topic :: Software Development :: Testing',
+                 'Topic :: Internet',
+                 'Topic :: Internet :: WWW/HTTP :: Site Management',
+                 'Topic :: Text Processing :: Markup :: HTML',
+                 'Intended Audience :: Developers'
+                 ],
 )
