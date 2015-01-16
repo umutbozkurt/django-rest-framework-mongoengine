bda46dc2017077f74953e13066a44a99e6d69554
Umut Bozkurt <umut@hipo.biz>
2015-01-16 12:14:11 +0200

    delete documentation build folder

--- Sample/settings.py
+++ Sample/settings.py
@@ -27,18 +27,8 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, '.','templates'),
-)
+TEMPLATE_LOADERS = ()
+TEMPLATE_DIRS = ()
 
 
 # Application definition
@@ -50,8 +40,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SampleApp',
-    'rest_framework'
+
+    'rest_framework',
+    'rest_framework_mongoengine',
+    'SampleApp'
 )
 
 MIDDLEWARE_CLASSES = (
