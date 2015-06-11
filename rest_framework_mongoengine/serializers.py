0a64ad68c9d62d3951cb7e3a69b0bb1ec851bbc6
andrea boetto <boettoandrea@gmail.com>
2015-06-11 16:08:43 +0200

    start work on fixing

--- rest_framework_mongoengine/serializers.py
+++ rest_framework_mongoengine/serializers.py
@@ -204,7 +204,7 @@ class DocumentSerializer(serializers.ModelSerializer):
 
         assert not (fields and exclude), "Cannot set both 'fields' and 'exclude'."
 
-        extra_kwargs = self._include_additional_options(extra_kwargs)
+        extra_kwargs = self.get_extra_
 
         # # Retrieve metadata about fields & relationships on the model class.
         info = get_field_info(model)
