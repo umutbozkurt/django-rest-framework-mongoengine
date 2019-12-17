e681bef2c1e3310a8ded3da412b096d026d0dc69
Betalos Oralyos <thoura.oussama@gmail.com>
2019-12-17 17:34:16 +0100

    fix validators (support latest drf)

--- rest_framework_mongoengine/validators.py
+++ rest_framework_mongoengine/validators.py
@@ -8,9 +8,9 @@ from rest_framework_mongoengine.repr import smart_repr
 
 
 class MongoValidatorMixin():
-    def exclude_current_instance(self, queryset):
-        if self.instance is not None:
-            return queryset.filter(pk__ne=self.instance.pk)
+    def exclude_current_instance(self, queryset, instance):
+        if instance is not None:
+            return queryset.filter(pk__ne=instance.pk)
         return queryset
 
 
@@ -19,10 +19,17 @@ class UniqueValidator(MongoValidatorMixin, validators.UniqueValidator):
 
     Used by :class:`DocumentSerializer` for fields, present in unique indexes.
     """
-    def __call__(self, value):
+    def __call__(self, value, serializer_field):
+        # Determine the underlying model field name. This may not be the
+        # same as the serializer field name if `source=<>` is set.
+        field_name = serializer_field.source_attrs[-1]
+        # Determine the existing instance, if this is an update operation.
+        instance = getattr(serializer_field.parent, 'instance', None)
+
         queryset = self.queryset
-        queryset = self.filter_queryset(value, queryset)
-        queryset = self.exclude_current_instance(queryset)
+        queryset = self.filter_queryset(value, queryset, field_name)
+        queryset = self.exclude_current_instance(queryset, instance)
+
         if queryset.first():
             raise ValidationError(self.message.format())
 
@@ -38,14 +45,20 @@ class UniqueTogetherValidator(MongoValidatorMixin, validators.UniqueTogetherVali
 
     Used by :class:`DocumentSerializer` for fields, present in unique indexes.
     """
-    def __call__(self, attrs):
+    def __call__(self, attrs, serializer_field):
         try:
             self.enforce_required_fields(attrs)
         except SkipField:
             return
+
+        field_name = serializer_field.source_attrs[-1]
+        # Determine the existing instance, if this is an update operation.
+        instance = getattr(serializer_field.parent, 'instance', None)
+
         queryset = self.queryset
-        queryset = self.filter_queryset(attrs, queryset)
-        queryset = self.exclude_current_instance(queryset)
+        queryset = self.filter_queryset(value, queryset, field_name)
+        queryset = self.exclude_current_instance(queryset, instance)
+
         # Ignore validation if any field is None
         checked_values = [
             value for field, value in attrs.items() if field in self.fields
